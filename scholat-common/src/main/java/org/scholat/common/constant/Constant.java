0588ec2c3ef17bd3969981066ae4e2739001b357
1726581875 <1726581875@qq.com>
2020-06-06 09:29:18 +0800

    删除以前遗留的spring-eureka

---
+++ scholat-common/src/main/java/org/scholat/common/constant/Constant.java
@@ -0,0 +1,13 @@
+package org.scholat.common.constant;
+
+/**
+ * @author xmz
+ * 2020年6月6日
+ * 一些常量
+ */
+public interface Constant {
+
+	public final static String TOKEN_NAME = "token";
+	
+	
+}
--- scholat-provider/scholat-provider-user/src/main/java/org/scholat/provider/user/controller/client/UserLoginClient.java
+++ scholat-provider/scholat-provider-user/src/main/java/org/scholat/provider/user/controller/client/UserLoginClient.java
@@ -1,8 +1,13 @@
 package org.scholat.provider.user.controller.client;
 
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.scholat.provider.user.pojo.UserLogin;
 import org.scholat.provider.user.service.UserLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,15 +21,24 @@ public class UserLoginClient {
 	@Autowired
 	private UserLoginService userLoginService;
 	
+	
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+	
+	
 	@PostMapping("/user/phone/login")
 	@HystrixCommand(fallbackMethod = "processHystrix_Get")
-	public Object loginByPhone(String phone,String password){
-		
+	public Object loginByPhone(String phone,String password,HttpServletResponse response){
+
 		UserLogin user = userLoginService.loginByPhone(phone, password);
-		
-		if(user==null){
-			throw new RuntimeException("啊啊1");
-		}
+		//随机生成一个token标识
+		String token = UUID.randomUUID().toString();
+		//存到redis
+		redisTemplate.opsForValue().set(token,user.getPassword(),3600);
+		//设置cookie
+		//setCookie(response,TOKEN_NAME, token, EXPIRE);
+	  //setCookie(response,"foolId",String.valueOf(fool.getFoolId()), EXPIRE);
+				
 		return user;
 	}
 	
