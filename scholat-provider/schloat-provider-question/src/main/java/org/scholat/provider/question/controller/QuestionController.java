3997b1ff788e04278a15fea64a5bfef84a70be41
xian <2448055384@qq.com>
2020-06-20 20:16:55 +0800

    添加了cookie及部分注释

--- scholat-provider/schloat-provider-question/src/main/java/org/scholat/provider/question/controller/QuestionController.java
+++ scholat-provider/schloat-provider-question/src/main/java/org/scholat/provider/question/controller/QuestionController.java
@@ -29,6 +29,7 @@ public class QuestionController {
 	@Autowired
 	private QuestionService questionService;
 	
+	//所以问题
 	@GetMapping("/question/allQuestion/{page}")
 	public PageQuestion findAllQuestion(@PathVariable Integer page){
 		PageQuestion pageQuestion = new PageQuestion();
@@ -40,34 +41,31 @@ public class QuestionController {
 		
 	}
 	
+	//删除问题
 	@DeleteMapping("/question/deleteQuestion/{questionId}")
 	public void deleteQuestion(@PathVariable Integer questionId){
 		questionService.deleteQuestion(questionId);
 	}
 	
+	//问题详情及该问题的回复
 	@GetMapping("/question/singleQuestion/{questionId}/{page}")
 	public SingleQueAndRepList findSingleQuestion(@PathVariable Integer questionId,@PathVariable Integer page){
 		PageRequest pageReques=PageRequest.of(page,8);  //第page+1页的8条记录
 		return questionService.findSingleQuestion(questionId,pageReques);
 	}
 	
-	@GetMapping("/question/questionListByUser/{page}")
-	public PageQuestion findQuestionByUser(@PathVariable Integer page){
+	//个人的全部提问的问题
+	@GetMapping("/question/questionListByUser/{userId}/{page}")
+	public PageQuestion findQuestionByUser(@PathVariable Integer userId,@PathVariable Integer page){
 		PageQuestion pagePersonQuestion = new PageQuestion();
 		PageRequest pageReques=PageRequest.of(page,8);  //第page+1页的8条记录		
-//		// Cookie cookie=new Cookie("sessionId","CookieTestInfo");
-//		Cookie[] cookies = (Cookie[]) request.getCookies();
-//		if(cookies != null){
-//		for(Cookie cookie : cookies){
-//		if(cookie.getName().equals("sessionId")){
-//			userId =  cookie.getValue();
-//		}
-		pagePersonQuestion.setQuestionList(questionService.findQuestionByTeacher(2,pageReques).getContent());   //此参数2仅作为测试数据，其实应是cookie的userId
-		pagePersonQuestion.setPageCount(questionService.findQuestionByTeacher(2,pageReques).getTotalPages());
-		pagePersonQuestion.setSize(questionService.findQuestionByTeacher(2,pageReques).getTotalElements());
+		pagePersonQuestion.setQuestionList(questionService.findQuestionByTeacher(userId,pageReques).getContent());   //此参数2仅作为测试数据，其实应是cookie的userId
+		pagePersonQuestion.setPageCount(questionService.findQuestionByTeacher(userId,pageReques).getTotalPages());
+		pagePersonQuestion.setSize(questionService.findQuestionByTeacher(userId,pageReques).getTotalElements());
 		return pagePersonQuestion;
 	}
 	
+	//发布新的提问
 	@PutMapping("/question/addQuestion")
 	public void addQuestion(Question question){
 		questionService.addQuestion(question);
--- scholat-provider/schloat-provider-question/src/main/java/org/scholat/provider/question/controller/client/QuestionClient.java
+++ scholat-provider/schloat-provider-question/src/main/java/org/scholat/provider/question/controller/client/QuestionClient.java
@@ -28,6 +28,7 @@ public class QuestionClient {
 	private QuestionService questionService;
 	
 
+	//获取全部问题
 	@GetMapping("/questions/allQuestion/{page}")
 	public Object findAllQuestion(@PathVariable Integer page){
 		PageQuestion pageQuestion = new PageQuestion();
@@ -38,12 +39,14 @@ public class QuestionClient {
 		return new ResultMsg<Object>(1,pageQuestion,"success");
 	}
 	
+	//删除提问的问题
 	@DeleteMapping("/questions/deleteQuestion/{questionId}")
 	public ResultMsg<Object> deleteQuestion(@PathVariable Integer questionId){
 		questionService.deleteQuestion(questionId);
 		return new ResultMsg<Object>(1, null,"success");
 	}
 	
+	//根据问题Id查找问题详情及该问题的回复的内容
 	@GetMapping("/questions/singleQuestion/{questionId}/{page}")
 	public ResultMsg<Object> findSingleQuestion(@PathVariable Integer questionId,@PathVariable Integer page){
 		PageRequest pageReques=PageRequest.of(page,8);  //第page+1页的8条记录
@@ -51,24 +54,19 @@ public class QuestionClient {
 		return new ResultMsg<Object>(1,singleQuestion,"success");
 	}
 	
-	@GetMapping("/questions/questionListByUser/{page}")
-	public Object findQuestionByUser(@PathVariable Integer page){
+	//个人发表的全部问题
+	@GetMapping("/questions/questionListByUser/{userId}/{page}")
+	public Object findQuestionByUser(@PathVariable Integer userId,@PathVariable Integer page){
 		PageQuestion pagePersonQuestion = new PageQuestion();
 		PageRequest pageReques=PageRequest.of(page,8);  //第page+1页的8条记录		
-//		// Cookie cookie=new Cookie("sessionId","CookieTestInfo");
-//		Cookie[] cookies = (Cookie[]) request.getCookies();
-//		if(cookies != null){
-//		for(Cookie cookie : cookies){
-//		if(cookie.getName().equals("sessionId")){
-//			userId =  cookie.getValue();
-//		}
-		pagePersonQuestion.setQuestionList(questionService.findQuestionByTeacher(2,pageReques).getContent());   //此参数2仅作为测试数据，其实应是cookie的userId
-		pagePersonQuestion.setPageCount(questionService.findQuestionByTeacher(2,pageReques).getTotalPages());
-		pagePersonQuestion.setSize(questionService.findQuestionByTeacher(2,pageReques).getTotalElements());
+		pagePersonQuestion.setQuestionList(questionService.findQuestionByTeacher(userId,pageReques).getContent());   //此参数2仅作为测试数据，其实应是cookie的userId
+		pagePersonQuestion.setPageCount(questionService.findQuestionByTeacher(userId,pageReques).getTotalPages());
+		pagePersonQuestion.setSize(questionService.findQuestionByTeacher(userId,pageReques).getTotalElements());
 		return new ResultMsg<Object>(1,pagePersonQuestion,"success");
 		
 	}
 	
+	//发新的提问
 	@PutMapping("/questions/addQuestion")
 	public ResultMsg<Object> addQuestion(@RequestBody Question question){
 		questionService.addQuestion(question);
